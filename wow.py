d23190d8eb16ee70d05d225bd6f4516523e0ee2f
Vhea He <vheahe.work@gmail.com>
2026-09-15 11:53:14 -0400

    c2 wow

--- wow.py
+++ wow.py
@@ -1,2 +1,2 @@
 print("Wow is a palindrome, and so is mom, and also wow is mom upside down. Say wow to your mom today")
-print("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+print("https://www.youtube.com/watch?v=dQw4w9WgXcQ")https://www.youtube.com/watch?v=dQw4w9WgXcQ
